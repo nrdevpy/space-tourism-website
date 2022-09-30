16e14a6b50d210713706a3a6f4c447da0532a17f
Nelson Rojas <nelson.rojas@playtech.com>
2022-09-30 12:16:22 +0200

    Change semantic nav and list from destination

--- js/fetch.js
+++ js/fetch.js
@@ -1,9 +1,11 @@
 // Data fetch.
 async function data () {
     const response = await fetch('../data.json');
-    const data = response.json()
+    const data = response.json();
     return data;
 }
 data().then(datas => {
-    
+    // console.log (datas)
 })
+
+export default data;
---
+++ js/planets.js
@@ -0,0 +1,11 @@
+// Data json.
+import data from './fetch.js'
+
+/* data().then(data => {
+    console.log(data.destinations[0])
+}) */
+
+// Upload data to the page.
+const planetsElement = document.getElementById('planets');
+const planet = document.getElementById('planet');
+const btn = document.getElementById('changePlanet');
