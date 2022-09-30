ed5789813dccd319419cbaa09b182e5d87f02b87
Nelson Rojas <nelson.rojas@playtech.com>
2022-09-30 13:12:38 +0200

    Get all planets data

--- js/planets.js
+++ js/planets.js
@@ -1,11 +1,23 @@
 // Data json.
 import data from './fetch.js'
 
-/* data().then(data => {
-    console.log(data.destinations[0])
-}) */
-
 // Upload data to the page.
 const planetsElement = document.getElementById('planets');
 const planet = document.getElementById('planet');
-const btn = document.getElementById('changePlanet');
+
+await data().then(data => {
+    const destinations = data.destinations
+    for (let planet of destinations) {
+        // Destructuring
+        const {
+            name,
+            image,
+            description,
+            distance,
+            travel
+        } = planet;
+        const {webp} = planet.images;
+
+        
+    }
+})
