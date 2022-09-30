9a38508f6f311b53f6d1bec6436aa0b9eec3b317
Nelson Rojas <nelson.rojas@playtech.com>
2022-09-30 13:54:43 +0200

    [Destination] Add planet image and planet list dinamically on

--- js/planets.js
+++ js/planets.js
@@ -2,22 +2,37 @@
 import data from './fetch.js'
 
 // Upload data to the page.
-const planetsElement = document.getElementById('planets');
+const planetsList = document.getElementById('planets');
 const planet = document.getElementById('planet');
 
 await data().then(data => {
     const destinations = data.destinations
+    console.log(destinations[0])
+    // Destructuring
+    const {
+        name,
+        image,
+        description,
+        distance,
+        travel
+    } = destinations[0];
+    const {webp} = destinations[0].images;
+    
+    // Planet image.
+    planet.setAttribute('src', `.${webp}`)
+    planet.setAttribute('alt', name)
+    
+    // Planets list.
+    let result = '';
     for (let planet of destinations) {
-        // Destructuring
-        const {
-            name,
-            image,
-            description,
-            distance,
-            travel
-        } = planet;
-        const {webp} = planet.images;
+        let {name} = planet;
+        name = name.toLowerCase();
 
-        
+        result += `
+            <li>
+                <button id="${name}" type="button">${name}</button>
+            </li>
+        `
     }
+    planetsList.innerHTML = result;
 })
