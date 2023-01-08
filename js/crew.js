0cf8342a226e887d6b8878210b1b049f3fb658f1
nr_dev <nelson_rojas_janda@hotmail.com>
2023-01-08 18:37:54 +0100

    Inner crew data to HTML

--- js/crew.js
+++ js/crew.js
@@ -2,5 +2,21 @@ import data from "./fetch.js";
 
 data().then(data => {
     const { crew } = data;
-    console.dir(crew[0])
+    const crewSection = document.querySelector('.crew__section');
+    let result = '';
+
+    for (let specialist of crew) {
+        let { bio, name, role } = specialist;
+        let { webp } = specialist.images;
+
+        result += `
+            <article>
+                <img class="crewImg" src=".${webp}">
+                <h3 class="lightGray">${role}</h3>
+                <h2>${name}</h2>
+                <p>${bio}</p>
+            </article>
+        `;
+        crewSection.innerHTML = result;
+    }
 })
