645ce932d6a0da99adc34d33bb00c8a7b3a69b97
nrdev <nelson_rojas_janda@hotmail.com>
2023-01-21 11:47:38 +0100

    Change crew styles

--- js/crews.js
+++ js/crews.js
@@ -6,17 +6,13 @@ const crewSection = document.querySelector('.crew__section')
 data().then(data => {
     const { crew } = data
     let innerSpecialist = ''
-    
+
     crew.forEach(specialist => {
         const { name, role, bio } = specialist
         const { webp } = specialist.images
         innerSpecialist += `
             <article class="crew__section__article">
-                <div class="">                
-                    <img class="crewImg" 
-                        src=".${webp}"
-                    >
-                </div>
+                <img class="crewImg" src=".${webp}"/>
                 <h3 class="lightGray">${role}</h3>
                 <h2>${name}</h2>
                 <p>${bio}</p>
