211128df1bdbfbfef18af52135de6a27b16b8234
nrdev <nelson_rojas_janda@hotmail.com>
2023-02-12 12:45:29 +0100

    Hide nav when click to any anchor

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const burgerCheck = document.getElementById('hamburger');
 const navStyle = document.getElementById('navbar').style;
+const navAnchorList = document.querySelectorAll('#navbar.navbar > li > a')
 
 burgerCheck.addEventListener('change', () => {
     let burgerIsCheck = burgerCheck.checked;
@@ -10,3 +11,9 @@ burgerCheck.addEventListener('change', () => {
         navStyle.right = '-100%';
     }
 });
+
+navAnchorList.forEach(anchor => {
+    addEventListener('click', () => {
+        navStyle.right = '-100%';
+    })
+})
