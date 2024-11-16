88c9322db2c7cbe6c4aa6d08b778cc01ebc33c25
nrdev <nelson_rojas_janda@hotmail.com>
2024-11-16 11:49:24 +0100

    Fix crew page

--- js/crews.js
+++ js/crews.js
@@ -12,35 +12,37 @@ data().then(data => {
     const { crew } = data
     let innerSpecialist = ''
     let bulletsPoints = ''
-    let specialistsNames = [];
+    let specialistsNames = []
 
     crew.forEach(specialist => {
         const { name, role, bio } = specialist
         const { webp } = specialist.images
         specialistsNames.push(name)
         innerSpecialist += `
-            <article id="${name.replace(/\s/g, "")}" data-name="${name}" class="crew__section__article desktop__flex">
-                <img class="crewImg" src=".${webp}"/>
+            <article id="${name.replace(/\s/g, "")}" data-name="${name}" class="crew__section__article">
+                <div class="container-width desktop__flex">
+                    <img class="crewImg" src=".${webp}"/>
+                    <div>
+                        <h3 class="lightGray">${role}</h3>
+                        <h2>${name}</h2>
+                        <p class="primaryColor">${bio}</p>
+                    </div>
                 <div>
-                    <h3 class="lightGray">${role}</h3>
-                    <h2>${name}</h2>
-                    <p class="primaryColor">${bio}</p>
-                </div>
             </article>
        `
         bulletsPoints += `
             <a href="#${name.replace(/\s/g, "")}" class="crew__bullet"></a>
        `
     })
-    crewSection.innerHTML = innerSpecialist;
-    bulletPointsList.innerHTML = bulletsPoints;
+    crewSection.innerHTML = innerSpecialist
+    bulletPointsList.innerHTML = bulletsPoints
 
 
     // Observer
     const observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
-            if (!entry.isIntersecting) return;
-            const dataName = entry.target.getAttribute('data-name');
+            if (!entry.isIntersecting) return
+            const dataName = entry.target.getAttribute('data-name')
             const specialistNumber = specialistsNames.findIndex(elem => elem === dataName)
             const bullets = document.querySelectorAll('.crew__bullet')
 
